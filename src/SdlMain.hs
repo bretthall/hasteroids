e8ece6adf5f2ea832c0f64dc7ed09d1601ae0672
Brett Hall <bretthall@fastmail.fm>
2015-04-30 21:16:38 -0700

    Refactored to eliminate IORef

--- src/SdlMain.hs
+++ src/SdlMain.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.IORef
 import Data.Time.Clock
 import Data.Word
 import Foreign.C.String
@@ -17,9 +16,8 @@ main :: IO ()
 main = do
     initRes <- SDL.init SDL_INIT_EVERYTHING
     putStrLn $ "Init res = " ++ (show initRes)
-
     title <- newCString "Test"
-    let (width, height) = (640, 480)
+    let (width, height) = (640, 640)
     win <- createWindow title 0 0 width height SDL.SDL_WINDOW_OPENGL
     glCreateContext win
     clearColor $= Color4 0 0 0 1
@@ -31,55 +29,53 @@ main = do
     ortho2D (-1.0) 1.0 (-1.0) 1.0
     matrixMode $= Modelview 0
     start <- getCurrentTime
-    state <- newIORef $ RS RotateNone 0.0 start
-    
-    let mainLoop = do
-          event <- alloca $ \event -> do
-                     res <- pollEvent event
-                     case res of
-                       1 -> peek event >>= return.Just
-                       _ -> return Nothing
-          case event of
-            Just (QuitEvent _ _) -> print "quit" >> return ()
-            Just (KeyboardEvent t _ _ _ _ k) -> handleKey t k state >> mainLoop
-            Nothing -> do
-              ct <- getCurrentTime
-              (RS d a t) <- readIORef state
-              let s' = RS d (updateAngle d a t ct) ct
-              writeIORef state s'
-              renderWindow s' win
-              mainLoop
-            _ -> mainLoop
-    mainLoop
-                    
+    mainLoop win $ RS RotateNone 0.0 start                    
     destroyWindow win
     quit
 
-handleKey :: Word32 -> Keysym -> IORef RotateState -> IO ()
+mainLoop :: Window -> RotateState -> IO ()
+mainLoop win state@(RS d a t) = do
+  event <- checkForEvent
+  case event of
+   Just (QuitEvent _ _) -> print "quit" >> return ()
+   Just (KeyboardEvent t _ _ _ _ k) -> handleKey t k state >>= mainLoop win
+   Nothing -> do
+     ct <- getCurrentTime
+     let state' = RS d (updateAngle d a t ct) ct
+     renderWindow state' win
+     mainLoop win state'
+   _ -> mainLoop win state
+
+checkForEvent :: IO (Maybe Event)
+checkForEvent = alloca $ \event -> do
+  res <- pollEvent event
+  case res of
+   1 -> peek event >>= return.Just
+   _ -> return Nothing
+   
+handleKey :: Word32 -> Keysym -> RotateState -> IO RotateState
 handleKey t key state | t == SDL_KEYDOWN = handleKeyDown key state
                       | t == SDL_KEYUP = handleKeyUp key state
-                      | otherwise = return ()
+                      | otherwise = return state
 
-handleKeyDown :: Keysym -> IORef RotateState -> IO ()
-handleKeyDown key state | keysymKeycode key == SDLK_LEFT = updateState RotateCCW
-                        | keysymKeycode key == SDLK_RIGHT = updateState RotateCW
-                        | otherwise = return ()
+handleKeyDown :: Keysym -> RotateState -> IO RotateState
+handleKeyDown key state@(RS d a t) | keysymKeycode key == SDLK_LEFT = updateState RotateCCW
+                                   | keysymKeycode key == SDLK_RIGHT = updateState RotateCW
+                                   | otherwise = return state
   where
     updateState dir = do
       ct <- getCurrentTime
-      modifyIORef state (\(RS d a t) -> RS dir (updateAngle d a t ct) ct)
+      return $ RS dir (updateAngle d a t ct) ct
 
-handleKeyUp :: Keysym -> IORef RotateState -> IO ()
-handleKeyUp key state | keysymKeycode key == SDLK_LEFT = updateState RotateCCW
-                      | keysymKeycode key == SDLK_RIGHT = updateState RotateCW
-                      | otherwise = return ()
+handleKeyUp :: Keysym -> RotateState -> IO RotateState
+handleKeyUp key state@(RS d a t) | keysymKeycode key == SDLK_LEFT = updateState RotateCCW
+                                 | keysymKeycode key == SDLK_RIGHT = updateState RotateCW
+                                 | otherwise = return state
   where
-    updateState dir = do
-      ct <- getCurrentTime
-      (RS d a t) <- readIORef state
-      if d == dir
-        then writeIORef state $ RS RotateNone (updateAngle d a t ct) ct
-        else return () 
+    updateState dir | dir == d = do
+                        ct <- getCurrentTime
+                        return $ RS RotateNone (updateAngle d a t ct) ct
+                    | otherwise = return state
         
 updateAngle :: RotateDir -> GLfloat -> UTCTime -> UTCTime -> GLfloat
 updateAngle d a t1 t2 = case d of
