477e7e3e2234540c49b0f376242c3530f630a499
Brett Hall <bretthall@fastmail.fm>
2016-03-08 22:25:09 -0800

    Added configuration file reading

--- src/SdlMain.hs
+++ src/SdlMain.hs
@@ -7,75 +7,101 @@ import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (($=))
 import qualified SDL
 import qualified Linear as L
-    
+import qualified Data.Configurator as Cfgr
+import qualified Data.Configurator.Types as CfgrTypes
+import Control.Exception.Base (catch)
+import System.Exit (exitFailure)
+
 data RotateDir = RotateNone | RotateCW | RotateCCW deriving Eq
 data RotateState = RS RotateDir GL.GLfloat DTC.UTCTime
-  
+
+data Config = Config {
+  windowWidth :: Int,
+  windowHeight :: Int,
+  playerAngularVelocity :: GL.GLfloat
+  } deriving Show
+
+readConfig :: IO Config
+readConfig = do
+  cfg <- catch (Cfgr.load [Cfgr.Required "hasteroids.config"]) handleMissingCfgr
+  wd <- Cfgr.lookupDefault 640 cfg "window.width" 
+  ht <- Cfgr.lookupDefault 480 cfg "window.height" 
+  pav <- Cfgr.lookupDefault 100 cfg "player.angular-velocity"
+  return (Config wd ht pav)
+    where
+      handleMissingCfgr :: CfgrTypes.ConfigError -> IO a
+      handleMissingCfgr err = do
+        print err
+        exitFailure
+              
 main :: IO ()
 main = do
-    SDL.initialize [SDL.InitVideo, SDL.InitEvents]
-    let (width, height) = (640, 640)
-    win <- SDL.createWindow "Hasteroids" $ 
-           SDL.defaultWindow {
-                    SDL.windowInitialSize = L.V2 width height,
-                    SDL.windowOpenGL = Just SDL.defaultOpenGL}
-    SDL.glCreateContext win
-    GL.clearColor $= GL.Color4 0 0 0 1
-    GL.lineWidth $= 1
-    GL.lineSmooth $= GL.Enabled
-    GL.viewport $= (GL.Position 0 0, GL.Size (fromIntegral width) (fromIntegral height))
-    GL.matrixMode $= GL.Projection
-    GL.loadIdentity
-    GL.ortho2D (-100.0) 100.0 (-100.0) 100.0
-    GL.matrixMode $= GL.Modelview 0
-    start <- DTC.getCurrentTime
-    mainLoop win $ RS RotateNone 0.0 start                    
-    SDL.destroyWindow win
-    SDL.quit
+  config <- readConfig
+  print config
+  SDL.initialize [SDL.InitVideo, SDL.InitEvents]
+  let (width, height) = ((fromIntegral $ windowWidth config), (fromIntegral $ windowHeight config))
+  win <- SDL.createWindow "Hasteroids" $ 
+         SDL.defaultWindow {
+           SDL.windowInitialSize = L.V2 width height,
+           SDL.windowOpenGL = Just SDL.defaultOpenGL}
+  SDL.glCreateContext win
+  GL.clearColor $= GL.Color4 0 0 0 1
+  GL.lineWidth $= 1
+  GL.lineSmooth $= GL.Enabled
+  GL.viewport $= (GL.Position 0 0, GL.Size width height)
+  GL.matrixMode $= GL.Projection
+  GL.loadIdentity
+  GL.ortho2D (-100.0) 100.0 (-100.0) 100.0
+  GL.matrixMode $= GL.Modelview 0
+  start <- DTC.getCurrentTime
+  mainLoop config win $ RS RotateNone 0.0 start                    
+  SDL.destroyWindow win
+  SDL.quit
 
-mainLoop :: SDL.Window -> RotateState -> IO ()
-mainLoop win state@(RS d a t) = do
+mainLoop :: Config -> SDL.Window -> RotateState -> IO ()
+mainLoop cfg win state@(RS d a t) = do
   event <- SDL.pollEvent
   case event of
    Just (SDL.Event _ SDL.QuitEvent) -> return ()
-   Just (SDL.Event _ (SDL.KeyboardEvent (SDL.KeyboardEventData _ t False k))) -> handleKey t k state >>= mainLoop win
+   Just (SDL.Event _ (SDL.KeyboardEvent (SDL.KeyboardEventData _ t False k))) ->
+     handleKey cfg t k state >>= mainLoop cfg win
    Nothing -> do
      ct <- DTC.getCurrentTime
-     let state' = RS d (updateAngle d a t ct) ct
+     let w = playerAngularVelocity cfg
+     let state' = RS d (updateAngle w d a t ct) ct
      renderWindow state' win
-     mainLoop win state'
-   _ -> mainLoop win state
+     mainLoop cfg win state'
+   _ -> mainLoop cfg win state
    
-handleKey :: SDL.InputMotion -> SDL.Keysym -> RotateState -> IO RotateState
-handleKey SDL.Pressed key state = handleKeyDown key state
-handleKey SDL.Released key state = handleKeyUp key state
+handleKey :: Config -> SDL.InputMotion -> SDL.Keysym -> RotateState -> IO RotateState
+handleKey cfg SDL.Pressed key state = handleKeyDown cfg key state
+handleKey cfg SDL.Released key state = handleKeyUp cfg key state
 
-handleKeyDown :: SDL.Keysym -> RotateState -> IO RotateState
-handleKeyDown key state@(RS d a t) | SDL.keysymKeycode key == SDL.KeycodeLeft = updateState RotateCCW
-                                   | SDL.keysymKeycode key == SDL.KeycodeRight = updateState RotateCW
-                                   | otherwise = return state
+handleKeyDown :: Config -> SDL.Keysym -> RotateState -> IO RotateState
+handleKeyDown cfg key state@(RS d a t) | SDL.keysymKeycode key == SDL.KeycodeLeft = updateState RotateCCW
+                                       | SDL.keysymKeycode key == SDL.KeycodeRight = updateState RotateCW
+                                       | otherwise = return state
   where
     updateState dir = do
       ct <- DTC.getCurrentTime
-      return $ RS dir (updateAngle d a t ct) ct
+      return $ RS dir (updateAngle w d a t ct) ct
+    w = playerAngularVelocity cfg
 
-handleKeyUp :: SDL.Keysym -> RotateState -> IO RotateState
-handleKeyUp key state@(RS d a t) | SDL.keysymKeycode key == SDL.KeycodeLeft = updateState RotateCCW
-                                 | SDL.keysymKeycode key == SDL.KeycodeRight = updateState RotateCW
-                                 | otherwise = return state
+handleKeyUp :: Config -> SDL.Keysym -> RotateState -> IO RotateState
+handleKeyUp cfg key state@(RS d a t) | SDL.keysymKeycode key == SDL.KeycodeLeft = updateState RotateCCW
+                                     | SDL.keysymKeycode key == SDL.KeycodeRight = updateState RotateCW
+                                     | otherwise = return state
   where
     updateState dir | dir == d = do
                         ct <- DTC.getCurrentTime
-                        return $ RS RotateNone (updateAngle d a t ct) ct
+                        return $ RS RotateNone (updateAngle w d a t ct) ct
                     | otherwise = return state
+    w = playerAngularVelocity cfg
 
-angVel :: GL.GLfloat
-angVel = 100.0
-                                  
-updateAngle :: RotateDir -> GL.GLfloat -> DTC.UTCTime -> DTC.UTCTime -> GL.GLfloat
-updateAngle d a t1 t2 = case d of
-                         RotateCCW -> a + diff*angVel
-                         RotateCW -> a - diff*angVel
+updateAngle :: GL.GLfloat -> RotateDir -> GL.GLfloat -> DTC.UTCTime -> DTC.UTCTime -> GL.GLfloat
+updateAngle w d a t1 t2 = case d of
+                         RotateCCW -> a + diff*w
+                         RotateCW -> a - diff*w
                          RotateNone -> a
   where
     diff = realToFrac $ DTC.diffUTCTime t2 t1    
