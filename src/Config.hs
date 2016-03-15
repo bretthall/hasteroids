234961856d577a0556b524d3cc7fb8815fb9a85c
Brett Hall <bretthall@fastmail.fm>
2016-03-14 21:52:22 -0700

    Config module and viewport fix
    Moved Config stuff into its own module
    Fixed viewport setup so that a square is now a square on screen

---
+++ src/Config.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Config (Config (..),
+               readConfig)
+       where
+
+import qualified Data.Configurator as Cfgr
+import qualified Data.Configurator.Types as CfgrTypes
+import qualified Graphics.Rendering.OpenGL as GL
+import Control.Exception.Base (catch)
+import System.Exit (exitFailure)
+
+data Config = Config {
+  windowWidth :: Int,
+  windowHeight :: Int,
+  playerAngularVelocity :: GL.GLfloat,
+  drawSquare :: Bool
+  } deriving Show
+
+readConfig :: IO Config
+readConfig = do
+  cfg <- catch (Cfgr.load [Cfgr.Required "hasteroids.config"]) handleMissingCfgr
+  wd <- Cfgr.lookupDefault 640 cfg "window.width" 
+  ht <- Cfgr.lookupDefault 480 cfg "window.height" 
+  pav <- Cfgr.lookupDefault 100 cfg "player.angular-velocity"
+  ds <- Cfgr.lookupDefault False cfg "debug.draw-square"
+  return (Config wd ht pav ds)
+    where
+      handleMissingCfgr :: CfgrTypes.ConfigError -> IO a
+      handleMissingCfgr err = do
+        print err
+        exitFailure
--- src/SdlMain.hs
+++ src/SdlMain.hs
@@ -7,33 +7,11 @@ import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (($=))
 import qualified SDL
 import qualified Linear as L
-import qualified Data.Configurator as Cfgr
-import qualified Data.Configurator.Types as CfgrTypes
-import Control.Exception.Base (catch)
-import System.Exit (exitFailure)
+import Config
 
 data RotateDir = RotateNone | RotateCW | RotateCCW deriving Eq
 data RotateState = RS RotateDir GL.GLfloat DTC.UTCTime
 
-data Config = Config {
-  windowWidth :: Int,
-  windowHeight :: Int,
-  playerAngularVelocity :: GL.GLfloat
-  } deriving Show
-
-readConfig :: IO Config
-readConfig = do
-  cfg <- catch (Cfgr.load [Cfgr.Required "hasteroids.config"]) handleMissingCfgr
-  wd <- Cfgr.lookupDefault 640 cfg "window.width" 
-  ht <- Cfgr.lookupDefault 480 cfg "window.height" 
-  pav <- Cfgr.lookupDefault 100 cfg "player.angular-velocity"
-  return (Config wd ht pav)
-    where
-      handleMissingCfgr :: CfgrTypes.ConfigError -> IO a
-      handleMissingCfgr err = do
-        print err
-        exitFailure
-              
 main :: IO ()
 main = do
   config <- readConfig
@@ -51,7 +29,9 @@ main = do
   GL.viewport $= (GL.Position 0 0, GL.Size width height)
   GL.matrixMode $= GL.Projection
   GL.loadIdentity
-  GL.ortho2D (-100.0) 100.0 (-100.0) 100.0
+  let orthoWidth = 100.0
+  let orthoHeight = orthoWidth * ((fromIntegral height) / (fromIntegral width))
+  GL.ortho2D (-orthoWidth) orthoWidth (-orthoHeight) orthoHeight
   GL.matrixMode $= GL.Modelview 0
   start <- DTC.getCurrentTime
   mainLoop config win $ RS RotateNone 0.0 start                    
@@ -69,7 +49,7 @@ mainLoop cfg win state@(RS d a t) = do
      ct <- DTC.getCurrentTime
      let w = playerAngularVelocity cfg
      let state' = RS d (updateAngle w d a t ct) ct
-     renderWindow state' win
+     renderWindow cfg state' win
      mainLoop cfg win state'
    _ -> mainLoop cfg win state
    
@@ -109,13 +89,16 @@ updateAngle w d a t1 t2 = case d of
 shipScaleFactor :: GL.GLfloat
 shipScaleFactor = 0.35
 
-renderWindow :: RotateState -> SDL.Window -> IO ()
-renderWindow (RS _ a _) win = do
+renderWindow :: Config -> RotateState -> SDL.Window -> IO ()
+renderWindow cfg (RS _ a _) win = do
   GL.clear [GL.ColorBuffer]
   GL.loadIdentity
-  GL.rotate a $ GL.Vector3 0.0 0.0 1.0
   GL.color $ GL.Color3 0.0 1.0 (0.0 :: GL.GLfloat)
   GL.scale shipScaleFactor shipScaleFactor 1.0
+  if (drawSquare cfg)
+    then GL.renderPrimitive GL.LineLoop $ mapM_ vertex2f [(-30.0, -30.0), (-30.0, 30.0), (30.0, 30.0), (30.0, -30.0) ]
+    else return ()
+  GL.rotate a $ GL.Vector3 0.0 0.0 1.0
   GL.renderPrimitive GL.LineLoop $ mapM_ vertex2f [(-5.0, -5.0), (0.0, 10.0), (5.0, -5.0), (0.0, 0.0) ]
   SDL.glSwapWindow win
 
